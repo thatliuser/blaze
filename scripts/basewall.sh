9d87b732fed965aae02af4093a4ebda108e74940
Eric L <eliu15@uci.edu>
2025-01-31 22:57:00 -0800

    Add scripts that were previously gitignored

---
+++ scripts/basewall.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+setup_logging() {
+    iptables -N INPUT_ACCEPT
+    iptables -N OUTPUT_ACCEPT
+
+    iptables -N INPUT_DROP
+    iptables -N OUTPUT_DROP
+
+    iptables -A INPUT_ACCEPT -j LOG --log-prefix "[INPUT_ACCEPT]"
+    iptables -A INPUT_ACCEPT -j ACCEPT
+
+    iptables -A OUTPUT_ACCEPT -j LOG --log-prefix "[OUTPUT_ACCEPT]"
+    iptables -A INPUT_ACCEPT -j ACCEPT
+
+    iptables -A OUTPUT_DROP -j LOG --log-prefix "[OUTPUT_DROP]"
+    iptables -A OUTPUT_DROP -j DROP
+
+    iptables -A INPUT_DROP -j LOG --log-prefix "[INPUT_DROP]"
+    iptables -A INPUT_DROP -j DROP
+}
+
+apply_basewall(){
+
+    iptables -P INPUT ACCEPT
+    iptables -P OUTPUT ACCEPT
+    iptables -P FORWARD ACCEPT
+
+    setup_logging
+
+    #examples:
+    #iptables -A INPUT -p tcp -m multiport -s xyz --dports xyz -j INPUT_ACCEPT
+    #iptables -A INPUT -p tcp -m multiport --dports xyz -j INPUT_DROP
+
+
+}
+
+remove_basewall() {
+    #examples:
+    #iptables -D INPUT -p tcp -m multiport -s xyz --dports xyz -j INPUT_ACCEPT
+    #iptables -D INPUT -p tcp -m multiport --dports xyz -j INPUT_DROP
+
+}
+
+if [ "$#" -eq 1 ]; then
+    echo 'Removing firewall'
+    remove_basewall
+else
+    echo 'Apply firewall'
+    apply_basewall
+fi
---
+++ scripts/lockdown.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+### DO NOT ADD THIS TO THE PUBLIC REPO
+PAM_PERMIT_PATH=$(find /lib/ -name "pam_permit.so" )
+PAM_DIR=$(dirname "$PAM_PERMIT_PATH")
+chattr +i $PAM_DIR/*
+chattr +i /etc/pam.d/*

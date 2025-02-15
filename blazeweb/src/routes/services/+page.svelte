d2809af7773719d2b54be273f5d5937721f4dd15
Eric L <eliu15@uci.edu>
2025-02-15 01:06:55 -0800

    Initial web interface with service and network "display"

---
+++ blazeweb/src/routes/services/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import { ClickableTile, DataTable } from "carbon-components-svelte";
+    import type { PageProps } from "./$types";
+    let { data }: PageProps = $props();
+    console.log(data);
+</script>
+
+{#each data.item as item}
+    <ClickableTile>
+        <h1>{item.cidr}</h1>
+        <DataTable
+            headers={[
+                { key: "key", value: "Key" },
+                { key: "value", value: "Value" }
+            ]}
+            rows={[
+                {
+                    id: "a",
+                    key: "Name",
+                    value: item.name
+                }
+            ]}
+        />
+    </ClickableTile>
+{/each}
---
+++ blazeweb/src/routes/services/+page.ts
@@ -0,0 +1,7 @@
+import type { PageLoad } from "$./types";
+
+export const load: PageLoad = async ({ fetch, params }) => {
+	const res = await fetch("/api/services");
+	const item = await res.json();
+	return { item };
+};

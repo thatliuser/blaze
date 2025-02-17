8514e0142b5abf3b55cf98bdd3a9eb29b44a00d3
Eric L <eliu15@uci.edu>
2025-02-16 18:10:38 -0800

    Rerun sqlx prepare and clean up API endpoints with generics

--- blazed/src/routes/mod.rs
+++ blazed/src/routes/mod.rs
@@ -10,10 +10,32 @@ use axum::{
         StatusCode, Uri,
         uri::{PathAndQuery, Scheme},
     },
+    response::{IntoResponse, Response, Result},
 };
 use futures::future::TryFutureExt;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+
+pub struct AppError(anyhow::Error);
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        (StatusCode::INTERNAL_SERVER_ERROR, self.0.to_string()).into_response()
+    }
+}
+
+impl<E> From<E> for AppError
+where
+    E: Into<anyhow::Error>,
+{
+    fn from(err: E) -> Self {
+        Self(err.into())
+    }
+}
+
+pub type AppResult<T> = Result<T, AppError>;
+
+pub type JsonResponse<T> = AppResult<Json<T>>;
 
 #[derive(Serialize, Deserialize)]
 pub struct CreateNetwork {
@@ -21,33 +43,19 @@ pub struct CreateNetwork {
     hosts: HashMap<String, Host>,
 }
 
-async fn do_create_network(pool: &sqlx::SqlitePool, network: CreateNetwork) -> anyhow::Result<()> {
-    for (_, host) in network.hosts.iter() {
-        host.insert(&network.network, pool).await?;
-    }
-    network.network.insert(pool).await?;
-    Ok(())
-}
-
 pub async fn create_network(
     State(state): State<AppState>,
     Json(network): Json<CreateNetwork>,
-) -> (StatusCode, String) {
-    match do_create_network(&state.pool, network).await {
-        Ok(()) => (StatusCode::OK, "".into()),
-        Err(err) => (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()),
+) -> AppResult<()> {
+    for (_, host) in network.hosts.iter() {
+        host.insert(&network.network, &state.pool).await?;
     }
+    network.network.insert(&state.pool).await?;
+    Ok(())
 }
 
-async fn do_get_networks(pool: &sqlx::SqlitePool) -> anyhow::Result<String> {
-    Ok(serde_json::to_string(&Network::all(pool).await?)?)
-}
-
-pub async fn get_networks(State(state): State<AppState>) -> (StatusCode, String) {
-    match do_get_networks(&state.pool).await {
-        Ok(json) => (StatusCode::OK, json),
-        Err(err) => (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()),
-    }
+pub async fn get_networks(State(state): State<AppState>) -> JsonResponse<Vec<Network>> {
+    Ok(Json(Network::all(&state.pool).await?))
 }
 
 #[derive(Serialize, Deserialize)]
@@ -69,9 +77,17 @@ pub struct QuotientServiceStatus {
     pub series: Vec<QuotientSeries>,
 }
 
-async fn do_import_quotient(pool: &sqlx::SqlitePool, host: &str) -> anyhow::Result<()> {
+#[derive(Serialize, Deserialize)]
+pub struct ImportQuotient {
+    pub uri: String,
+}
+
+pub async fn import_quotient(
+    State(state): State<AppState>,
+    Query(query): Query<ImportQuotient>,
+) -> AppResult<()> {
     let slug = PathAndQuery::from_static("/api/graphs/services");
-    let uri: Uri = host.parse()?;
+    let uri: Uri = query.uri.parse()?;
     // If there's no URI scheme, try HTTP first, then HTTPS
     let resp = if uri.scheme().is_none() {
         println!("No URI scheme, trying http/https");
@@ -112,34 +128,10 @@ async fn do_import_quotient(pool: &sqlx::SqlitePool, host: &str) -> anyhow::Resu
             "none!",
             "unknown"
         )
-        .execute(pool)
+        .execute(&state.pool)
         .await?;
     }
     Ok(())
-}
-
-#[derive(Serialize, Deserialize)]
-pub struct ImportQuotient {
-    pub uri: String,
-}
-pub async fn import_quotient(
-    State(state): State<AppState>,
-    query: Query<ImportQuotient>,
-) -> (StatusCode, String) {
-    println!("Running import quotient");
-    match do_import_quotient(&state.pool, &query.uri).await {
-        Ok(()) => (StatusCode::OK, "".into()),
-        Err(err) => (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()),
-    }
-}
-
-// TODO: There's more to this but IDRC about the stuff rn
-struct DbService {
-    pub name: String,
-    pub info: String,
-    pub kind: String,
-    pub cidr: Option<String>,
-    pub ip: Option<String>,
 }
 
 #[derive(Deserialize, Serialize)]
@@ -147,21 +139,14 @@ pub struct Service {
     pub name: String,
 }
 
-async fn do_get_services(pool: &sqlx::SqlitePool) -> anyhow::Result<String> {
-    let services: Vec<_> = sqlx::query_as!(DbService, "SELECT * FROM Services")
-        .fetch_all(pool)
+pub async fn get_services(State(state): State<AppState>) -> JsonResponse<Vec<Service>> {
+    let services: Vec<_> = sqlx::query!("SELECT * FROM Services")
+        .fetch_all(&state.pool)
         .await?
         .iter()
         .map(|service| Service {
             name: service.name.clone(),
         })
         .collect();
-    Ok(serde_json::to_string(&services)?)
-}
-
-pub async fn get_services(State(state): State<AppState>) -> (StatusCode, String) {
-    match do_get_services(&state.pool).await {
-        Ok(json) => (StatusCode::OK, json),
-        Err(err) => (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()),
-    }
+    Ok(Json(services))
 }
